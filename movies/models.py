48dbb3288d4554b702614e9f99df9617a47a6418
Teran326 <swysog@gmail.com>
2021-03-21 21:37:10 +0100

    genre

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="Genre name", help_text='Enter a film genre (e.g. sci-fi, comedy)')
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
